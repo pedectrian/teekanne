b62b4453c067fcad89e493ddfe408d1ab508f4fc
Alexander Permyakov <pedectrian@ruwizards.com>
2014-06-25 23:27:01 +0400

    slider fix

--- front-page.php
+++ front-page.php
@@ -71,7 +71,7 @@ if ('posts' == get_option('show_on_front') && onepage_get_option('re_nm') !== 'o
                     <h2 class="list-title">НАШИ ЧАИ:</h2>
                     <ul class="tea-list">
                         <?php
-                        $slides = [0, 5, 2, 3, 2, 2];
+                        $slides = array(0, 5, 2, 3, 2, 2);
                         $slide = 0;
                         $i = 0;
                         if ( $history->have_posts() ) {
